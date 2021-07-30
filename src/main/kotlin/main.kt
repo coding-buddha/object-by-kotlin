7730671cae915ba4931faed0d68def1b002a1c35
PARK SUNG DONG <pasudo123@naver.com>
2021-07-30 23:13:30 +0900

    object 책 한번 훑어봄..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
+import kotlin.math.ceil
+import kotlin.math.sqrt
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    for(index in 1..10) {
+        if(index == 5) {
+            continue
+        }
+        println("=> $index")
+    }
 }
